83b9b7956f644b7cb8d40d003869a6132e1e5211
Todd Peterson <petetodd@isu.edu>
2020-07-01 13:35:03 -0600

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -65,3 +65,14 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(ingredient), ingredient.name)
+
+    def test_recipe_str(self):
+        """Test the recipe string representation"""
+        recipe = models.Recipe.objects.create(
+            user=sample_user(),
+            title='Steak and bake',
+            time_minutes=30,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
